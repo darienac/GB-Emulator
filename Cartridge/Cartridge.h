3fcd437ddd26971f3e0af8c36c8eeb425c9173c3
tyson314 <tysonpeterson4@gmail.com>
2024-02-22 16:09:35 -0700

    cartridge

---
+++ Cartridge/Cartridge.h
@@ -0,0 +1,55 @@
+//
+// Created by Tyson Peterson on 2/14/24.
+//
+#include <string>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+
+#ifndef GAMEBOY_EMULATOR_CARTRIDGE_H
+#define GAMEBOY_EMULATOR_CARTRIDGE_H
+
+using namespace std;
+
+class Cartridge {
+private:
+    char* cartData;
+    int cartSize;
+
+    /*
+     * Basic Cartridge info... Needed only to
+     * confirm ROM is being read
+     */
+    string romTitle;
+    string romType;
+    string licensee;
+    int romSize;
+public:
+    /*
+     * Pass in relative file path,
+     * loads the byte data into cartData
+     */
+    Cartridge(string fileName);
+
+    /*
+     * Prints simple debugging info
+     */
+    void printHeaderInfo();
+
+    /*
+     * Takes a 2 byte address (offset)
+     * and writes the passed in byte to cartData
+     * We probably won't use this until we test
+     * cartridges with MEM banks or RAM.
+     */
+    void cartWrite(uint16_t addr, uint8_t byte);
+
+    /*
+     * Takes a 2 byte address (offset)
+     * and reads the associated byte from cartData
+     */
+    uint8_t cartRead(uint16_t addr);
+};
+
+
+#endif //GAMEBOY_EMULATOR_CARTRIDGE_H
