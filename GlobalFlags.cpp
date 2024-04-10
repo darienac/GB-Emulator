e86c9570e11870558380e2379fcf7a95cc61d7e2
tyson314 <tysonpeterson4@gmail.com>
2024-04-10 08:29:10 -0600

    bug fixes -- hc flag in many operations, call logic fixes, misc bug fixes

--- GlobalFlags.cpp
+++ GlobalFlags.cpp
@@ -4,9 +4,9 @@
 
 #include "GlobalFlags.h"
 
-const bool GlobalFlags::debug = true;
+const bool GlobalFlags::debug = false;
 const bool GlobalFlags::manualdbg = false;
-const bool GlobalFlags::showRegisters = true;
-const bool GlobalFlags::checkForStuckInstruction = true;
+const bool GlobalFlags::showRegisters = false;
+const bool GlobalFlags::checkForStuckInstruction = false;
 const bool GlobalFlags::showControls = true;
 const bool GlobalFlags::matchTranscript = true;
